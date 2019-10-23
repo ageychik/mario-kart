844dc7e5025197ba9cca8fd075bcf4459abb2a84
Roma <forwm.ru@gmail.com>
2019-10-23 20:39:11 +0300

    minimized images, improved performance

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@ const sass = require('gulp-sass');
 const concat = require('gulp-concat');
 const sourcemaps = require('gulp-sourcemaps');
 const autoprefixer = require('gulp-autoprefixer');
+const spritesmith = require('gulp.spritesmith');
+
 
 
 gulp.task('sass', function () {
@@ -15,6 +17,13 @@ gulp.task('sass', function () {
         .pipe(gulp.dest('./public/css'));
 });
 
+gulp.task('sprite', function () {
+    const spriteData = gulp.src('./public/images/*.png').pipe(spritesmith({
+        imgName: 'sprite.png',
+        cssName: 'sprite.css'
+    }));
+    return spriteData.pipe(gulp.dest('public/images/sprite'));
+});
 
 
 gulp.task('default', function (){
